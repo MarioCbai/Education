975e20098f65977065d0a7e05411cf38ee580ec8
1459831498@qq.com <1459831498@qq.com>
2021-01-13 11:42:12 +0800

    55

--- Education/EducationDAL/EssentialData/ClasstypeManagements/ClasstypeManagementRealize.cs
+++ Education/EducationDAL/EssentialData/ClasstypeManagements/ClasstypeManagementRealize.cs
@@ -15,12 +15,12 @@ namespace EducationDAL.EssentialData.ClasstypeManagements
 
         public override List<ClassModelMod_BusinessTypeMod> ClasstypeManagementShow()
         {
-            return DapperHelper.Query<ClassModelMod_BusinessTypeMod>("select * from ClassModelMod a join BusinessTypeMod b on a.BusinessTypeId=b.BusinessTypeId", "");
+            return DapperHelper.Query<ClassModelMod_BusinessTypeMod>("select * from SubjectsHourType a join ClassModel b on a.SHId=b.ClassModelId join Subjects c on a.SHId=c.SubjectsId","");
         }
 
         public override int ClasstypeManagementUpt(ClassModelMod_BusinessTypeMod c)
         {
-            
+            return DapperHelper.Execute("update SubjectsHourType set HourTypeName=@HourTypeName,Sort=@Sort,Proportion=@Proportion where HourTypeId=@HourTypeId", c);
         }
 
         public override int ClasstypeManagementZtai(int ztai, int id)
