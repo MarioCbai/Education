314e9fadabdd8e3d7ca4fceef3b85514ee069d00
增威 韩 <2404545995@qq.com>
2021-01-15 11:51:10 +0800

    韩

--- Education/EducationBLL/InstitutionManagementBll.cs
+++ Education/EducationBLL/InstitutionManagementBll.cs
@@ -1,4 +1,5 @@
 ﻿using EducationDAL.InstitutionManagement;
+using EducationMODEL.organizational;
 using IEducation;
 using System;
 using System.Collections.Generic;
@@ -10,5 +11,32 @@ namespace EducationBLL
     public  class InstitutionManagementBll: IInstitutionManagement
     {
         Institution institutions = new InstitutionPlant();
+        #region 机构管理
+        //机构管理显示
+        public List<OrganMod> GetOrganMods()
+        {
+            return institutions.Organ().GetOrganMods();
+        }
+        //添加机构管理
+        public int AddOrganes(OrganMod organ)
+        {
+            return institutions.Organ().AddOrganes(organ);
+        }
+        //反填机构管理信息        
+        public OrganMod ModiyIdOrganes(int orgid)
+        {
+            return institutions.Organ().ModiyIdOrganes(orgid);
+        }
+        //修改机构管理信息
+        public int ModiyOrganes(OrganMod organ)
+        {
+            return institutions.Organ().ModiyOrganes(organ);
+        }
+        //单删除机构管理信息
+        public int DeleteOrganes(int ids)
+        {
+            return institutions.Organ().DeleteOrganes(ids);
+        }
+        #endregion
     }
 }
--- Education/IEducation/IInstitutionManagement.cs
+++ Education/IEducation/IInstitutionManagement.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EducationMODEL.organizational;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,5 +8,17 @@ namespace IEducation
     //机构管理
     public interface IInstitutionManagement
     {
+        #region 机构管理
+        //机构管理显示
+        List<OrganMod> GetOrganMods();
+        //添加机构信息
+        int AddOrganes(OrganMod organ);
+        //反填机构管理信息
+        OrganMod ModiyIdOrganes(int orgid);
+        //修改机构管理信息
+        int ModiyOrganes(OrganMod organ);
+        //单删除机构管理信息
+        int DeleteOrganes(int ids);
+        #endregion
     }
 }
