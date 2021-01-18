388abc2cd7ef2e9c9463402d0f38860b038d3ff3
1173090905@qq.com <1173090905@qq.com>
2021-01-18 14:58:33 +0800

    郭

--- Education/IEducation/ITeachingManagement.cs
+++ Education/IEducation/ITeachingManagement.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EducationMODEL.linkModel;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,5 +8,7 @@ namespace IEducation
     //教学管理
     public interface ITeachingManagement
     {
+        //课堂管理显示
+        public abstract List<ClassroomManagement> ManagementShow();
     }
 }
